7c4f12b870d078ba1b2e21ea276c476a913e24c5
msfarookhy <msfarookhy@gmail.com>
2022-04-27 19:09:41 +0300

    online shoping Cart

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+const router = express.Router();
+import {
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  getProductById,
+  getProducts,
+  createProductReview,
+  getTopProducts,
+} from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
+router.get("/top", getTopProducts);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, deleteProduct)
+  .put(protect, admin, updateProduct);
+
+export default router;
